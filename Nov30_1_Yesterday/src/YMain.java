46595f21f72fe8a686eead9ec2414569313e6b07
이효섭 <64147276+leehyoseop@users.noreply.github.com>
2021-12-10 09:37:35 +0900

    Add files via upload
    11.30.java

---
+++ Nov30_1_Yesterday/src/YMain.java
@@ -0,0 +1,12 @@
+
+public class YMain {
+	public static void main(String[] args) throws InterruptedException {
+		System.out.printf("집까지 걸린 시간\t: %d시간 %d분\n", 1, 20);
+		System.out.println("저녁은 타코를 먹었습니다");
+		System.out.printf("%s","집에 도착해서 장본것 정리하고 빨래돌렸습니다\n");
+		System.out.printf("취침 시간\t: %s %d시간 %d분\n", "새벽", 12, 45);
+		System.out.println("자바를 다시 시작하니깐 오랜만에 해서 그런지 재밌습니다");
+		
+		Thread.sleep(5000);
+	}
+}
